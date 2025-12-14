d819758e774399ffc31542223a14220f9466552e
Riff-Tone <148513630+Riff-Tone@users.noreply.github.com>
2025-12-14 13:45:45 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
    int x(0);
-
+   int y(2);
    std::cout << x << std::endl;
 }
